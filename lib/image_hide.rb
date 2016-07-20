2edc3da51d941cfc5fc505072a8a90a64a131445
Hassek <tomas@onereceipt.me>
2016-07-20 00:18:41 -0400

    unit tested and refactored function to set image inside another image

--- lib/image_hide.rb
+++ lib/image_hide.rb
@@ -7,92 +7,109 @@ module ChunkType
 end
 
 class ImageHide
+  REQUIRED_METHODS = [:read, :pos, :tell, :rewind, :binmode, :binmode?, :write]
+
   attr_accessor :image
 
-  def initialize(image)
-    @image_path = image
-    @image = File.open(image, 'rb+')
-  end
+  private
 
-  def is_png?(image)
-    return true
-  end
-
-  def generate_crc(token, image_bytes)
-    return Zlib::crc32("#{token}#{image_bytes}")
-  end
-
-  # Hides the given image into the base image
-  def set_hidden_image(hidden_image)
-    if not (self.is_png? hidden_image)
-      # convert image to png if possible
-    end
-
-    token = self.seek_token
-    if token != ChunkType::END_TOKEN
-      puts("image already has a hidden image")
-      return false
-    end
-
-    byte_pos = @image.pos
-    @image.pos = 0
-    # don't save to file, return a StringIO instead
-    merged_image = StringIO.new
-    merged_image.write(@image.read())
-
-    hidden_bytes = hidden_image.read()
-    crc = self.generate_crc(ChunkType::HIDDEN_TOKEN, hidden_bytes)
-
-    # write chunk size as big endian integer (!i)
-    length = [hidden_bytes.unpack("C*").length].pack('!i')
-
-    # TODO Refactor this pretty plox
-    merged_image.pos = byte_pos
-    merged_image.write(length)
-    merged_image.write(ChunkType::HIDDEN_TOKEN)
-    merged_image.write(hidden_bytes)
-    merged_image.write([crc].pack('!i'))
-    merged_image.write([0].pack('!i'))
-    merged_image.write(ChunkType::END_TOKEN)
-
-    return merged_image
-  end
-
-  # Extracts the hidden image and returns it as a valid PNG
-  def get_hidden_image(image)
-    if self.seek_token(image) == ChunkType::HIDDEN_TOKEN
-
-    end
-  end
-
-  # returns a HIDDEN_TOKEN if found, else, an END_TOKEN
-  # and leaves the image in the byte where the token was found
-  def seek_token(image=@image)
-    # restart file read position so the function becomes idempotent, also, skip first 8 bytes
-    @image.pos = 8
-    chunk_type = nil
-
-    while chunk_type != ChunkType::END_TOKEN
-      # find a more readable way to get the hex number
-      # please read here if you are curious about pack/unpack http://blog.bigbinary.com/2011/07/20/ruby-pack-unpack.html
-      chunk_size = @image.read(4).unpack('H*').first.to_i(16)
-      chunk_type = @image.read(4).encode("ASCII")
-
-      puts("Chunk Size: #{chunk_size}")
-      puts("Chunk Type: #{chunk_type}")
-
-      if chunk_type == ChunkType::HIDDEN_TOKEN
-        return ChunkType::HIDDEN_TOKEN
+    def sanitize(image)
+      # if it doesn't implements this methods, assume is a file path
+      if not (REQUIRED_METHODS.all? {|method| image.respond_to?(method)})
+        image = File.open(image, 'rb+')
       end
 
-      @image.read(chunk_size)
-      crc = @image.read(4).unpack('H*').first.to_i(16)
+      image = image
+      image.binmode.rewind
 
-      puts("CRC: #{crc}")
+      return image
     end
 
-    # setup position so we can write just before the END_TOKEN
-    @image.pos = (@image.pos - 8)
-    return ChunkType::END_TOKEN
-  end
+  public
+
+    def initialize(image)
+      @image = sanitize(image)
+    end
+
+    # TODO to be implemented
+    def is_png?(image)
+      return true
+    end
+
+    def generate_crc(token, image_bytes)
+      return [Zlib::crc32("#{token}#{image_bytes}")].pack('!i')
+    end
+
+    # Hides the given image into the base image
+    def set_hidden_image(hidden_image)
+      hidden_image = sanitize(hidden_image)
+
+      # XXX do we need the hidden image to be PNG?
+      if not (self.is_png? hidden_image)
+        # convert image to png if possible
+      end
+
+      token = self.seek_token
+      if token != ChunkType::END_TOKEN
+        # XXX change this for a logger warning?
+        puts("image already has a hidden image")
+        return false
+      end
+
+      hidden_bytes = hidden_image.read()
+      crc = self.generate_crc(ChunkType::HIDDEN_TOKEN, hidden_bytes)
+
+      # write chunk size as big endian integer (!i)
+      length = [hidden_bytes.unpack("C*").length].pack('!i')
+
+      ordered_data = [length, ChunkType::HIDDEN_TOKEN, hidden_bytes, crc,
+       [0].pack('!i'), ChunkType::END_TOKEN]
+
+      ordered_data.each { |data| @image.write(data)}
+
+      @image.rewind
+      return @image
+    end
+
+    # TODO to be implemented
+    # Extracts the hidden image and returns it as a valid PNG
+    def get_hidden_image(image)
+      if self.seek_token(image) == ChunkType::HIDDEN_TOKEN
+
+      end
+    end
+
+    # returns a HIDDEN_TOKEN if found, else, an END_TOKEN
+    # and leaves the image in the byte where the token was found
+    def seek_token(image=@image)
+      # restart file read position so the function becomes idempotent, also, skip first 8 bytes
+      @image.pos = 8
+      chunk_type = nil
+
+      while chunk_type != ChunkType::END_TOKEN
+        # find a more readable way to get the hex number
+        # please read here if you are curious about pack/unpack http://blog.bigbinary.com/2011/07/20/ruby-pack-unpack.html
+        chunk_size = @image.read(4).unpack('H*').first.to_i(16)
+        chunk_type = @image.read(4).encode("ASCII")
+
+        puts("Chunk Size: #{chunk_size}")
+        puts("Chunk Type: #{chunk_type}")
+        if chunk_type == "IDAT"
+          require 'pry-byebug'; binding.pry
+        end
+
+        if chunk_type == ChunkType::HIDDEN_TOKEN
+          return ChunkType::HIDDEN_TOKEN
+        end
+
+        @image.read(chunk_size)
+        crc = @image.read(4).unpack('H*').first.to_i(16)
+
+        puts("CRC: #{crc}")
+      end
+
+      # setup position so we can write just before the END_TOKEN
+      @image.pos = (@image.tell - 12)
+      return ChunkType::END_TOKEN
+    end
 end
--- test/image_hide_test.rb
+++ test/image_hide_test.rb
@@ -1,11 +1,20 @@
+require "image_hide"
 require 'test/unit'
 
 class ImageHideUT < Test::Unit::TestCase
   def setup
+    @base_image = File.open('test/images/base_image.png', 'rb+')
+    @hidden_image = 'test/images/hidden_image.png'
   end
 
   def test_set_hidden_image
-    pass
+    # XXX need to create an idempotent unit test
+    image_hide = ImageHide.new @base_image
+    assert_equal(image_hide.seek_token, ChunkType::END_TOKEN)
+
+    merged_image = image_hide.set_hidden_image @hidden_image
+    merged_image = ImageHide.new merged_image
+    assert_equal(merged_image.seek_token, ChunkType::HIDDEN_TOKEN)
   end
 
   def test_extract_hidden_image
